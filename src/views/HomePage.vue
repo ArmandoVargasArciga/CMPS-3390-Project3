86696f05ada5899d4e586c9acc66381e3c78b23e
Gael <gaelcarrillo995@gmail.com>
2025-11-14 19:25:23 -0800

    got the correct text box in, fixed it to the correct landing page.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,22 @@
 <template> 
  <div class = "container"> 
     <!--- Ape typing.com--->
- <h2> APETYPE </h2> 
+ <h2 class="title"> APETYPE </h2> 
 
- <v-text-field label="Name"> <v-text-field>
- <v-text-field label="Password"> <v-text-field>
+
+ <input placeholder="test holder"/>
+ 
+ 
+ <!--These below dont work, youtube video clutch
+ <v-text-field label="Name" class="textBox"> </v-text-field>
+ <v-text-field label="Password" class="textBox"> </v-text-field>
+ -->
     
 <!--Email mabey-->
- <v-text-field label="Email Address" placeholder="johnpork@gmail.com" type="email"> <v-text-field>
-
+ <v-text-field label="Email Address" placeholder="johnpork@gmail.com" type="email"> </v-text-field>
     <v-btn rounded="lg" size="large"> Login </v-btn>
     <v-btn rounded="lg" size="large"> Sign Up </v-btn>
-<div>
+ </div>
 
 </template>
 
@@ -20,6 +25,14 @@
 
 </script>
 <!--Have classes names to organize the style of the front page. -->
+
+
 <style scoped> 
+
+.textBox{
+   color: black;
+
+}
+
 
 </style>
