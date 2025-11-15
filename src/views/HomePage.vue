7fabf5f8da97a904d0aead84ff5f5d7d20ec04ab
Gael <gaelcarrillo995@gmail.com>
2025-11-14 20:05:02 -0800

    why am I having errros in adding the main picture in the homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,24 +4,28 @@
  <h2 class="title"> APETYPE </h2> 
 
 
- <input placeholder="test holder"/>
+ <input placeholder="UserName"/>
+ <input placeholder="Password"/>
  
  
  <!--These below dont work, youtube video clutch
  <v-text-field label="Name" class="textBox"> </v-text-field>
  <v-text-field label="Password" class="textBox"> </v-text-field>
  -->
-    
+   </div> 
 <!--Email mabey-->
- <v-text-field label="Email Address" placeholder="johnpork@gmail.com" type="email"> </v-text-field>
-    <v-btn rounded="lg" size="large"> Login </v-btn>
-    <v-btn rounded="lg" size="large"> Sign Up </v-btn>
+<div class="inputLogin">
+ <v-text-field label="Email Address" placeholder="johnpork@gmail.com" type="email"/>
+    <v-btn rounded="lg" size="large" class="login"> Login </v-btn>
+    <v-btn rounded="lg" size="large" class="SignUp"> Sign Up </v-btn>
  </div>
 
+
+<img :src="mainImage"/>
 </template>
 
 <script setup>
-
+import mainImage from '@/src/images/content.png'
 
 </script>
 <!--Have classes names to organize the style of the front page. -->
