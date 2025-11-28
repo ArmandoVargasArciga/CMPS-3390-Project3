2106a1a8f86deeefea348d8427f7751459c26089
Armando <vargas.a2004@gmail.com>
2025-11-28 15:05:02 -0800

    Template for signup page done

--- src/views/signUp.vue
+++ src/views/signUp.vue
@@ -3,9 +3,20 @@
 </script>
 
 <template>
-    <form @submit.prevent="">
+    <form @submit.prevent="signUp">
+        <label for="firstName">First Name: </label>
+        <input v-model="firstName"><br>
+        <label for="lastName">Last Nmae: </label>
+        <input v-model="lastName"><br>
         <label for="username">Username: </label>
-        <input v-model="username">
+        <input v-model="username"><br>
+        <label for="email">Email: </label>
+        <input v-model="email"><br>
+        <label for="password">Password: </label>
+        <input v-model="password"><br>
+
+        <button type="submit">Sign Up!</button>
+        
     </form>
 </template>
 
