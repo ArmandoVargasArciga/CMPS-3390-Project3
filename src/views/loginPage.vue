9a24c12c21e0d2132bb1736e98fe70890e8d26f3
Armando <vargas.a2004@gmail.com>
2025-11-28 17:24:57 -0800

    login works now just need to do the routing between pages now

---
+++ src/views/loginPage.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const email = ref('');
+const password = ref('');
+
+const logIn = async() => {
+    try{
+        const res = await axios.post("http://localhost:3000/login", {
+            email: email.value,
+            password: password.value
+    })
+    localStorage.setItem('token', res.data.token)
+    alert('Login successful!')
+    } catch(e){
+        alert("Login failed: " + e.response.data.error)
+    }
+}
+</script>
+
+<template>
+    <form @submit.prevent="logIn">
+        <label for="email">Email: </label>
+        <input v-model="email"><br>
+        <label for="password">Password: </label>
+        <input v-model="password"><br>
+
+        <button type="submit">Log in!</button>
+
+    </form>
+</template>
+
+<style>
+
+</style>
